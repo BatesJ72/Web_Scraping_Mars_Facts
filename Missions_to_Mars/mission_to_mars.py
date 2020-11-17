11236d99c10e0535cbe3dfb538a1d1718d02a8c8
Jessica Bates <bates.jessica72@gmail.com>
2020-11-16 20:09:43 -0600

    FUCK YEAH I GOT THE RIGHT HEADLINE

--- Missions_to_Mars/mission_to_mars.py
+++ Missions_to_Mars/mission_to_mars.py
@@ -1,35 +1,50 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd 
+import pandas as pd
+from selenium import webdriver
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
+# Selenium
+def get_html(url, wait):
+        driver = webdriver.Firefox()
+        driver.get(url)
+        driver.implicitly_wait(wait)
+        html = driver.page_source
+        driver.close
+        return html
 
 
 # NASA Mars News
 
-URL_NMN = "https://mars.nasa.gov/news/"
+# URL_NMN = "https://mars.nasa.gov/news/"
+url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
+html_nmn = get_html(url, wait = 1)
+# BASE_PARAMS_NMN = {
+#     "page": 0,
+#     "per_page": 40,
+#     "order": "publish_date+desc%2Ccreated_at+desc",
+#     # "search": ,
+#     "category": "19%2C165%2C184%2C204",
+#     "blank_scope": "Latest",
+# }
 
-BASE_PARAMS_NMN = {
-    "page": 0,
-    "per_page": 40,
-    "order": "publish_date+desc%2Ccreated_at+desc",
-    # "search": ,
-    "category": "19%2C165%2C184%2C204",
-    "blank_scope": "Latest",
-}
-
-# r_nmn = requests.get(URL)
-r_nmn = requests.get(URL_NMN, params = BASE_PARAMS_NMN)
+# r_nmn = requests.get(URL_NMN)
+# r_nmn = requests.get(URL_NMN, params = BASE_PARAMS_NMN)
 
 # print(r_nmn.status_code)
 # print(r_nmn.text)
 
-soup_nmn = BeautifulSoup(r_nmn.text, "html.parser")
+
+
+
+soup_nmn = BeautifulSoup(html_nmn, "html.parser")
 # print(soup_nmn.prettify())
 # print(soup_nmn.find(class_="content_title").text.strip())
-titles = soup_nmn.find_all(class_="content_title")
-title = titles[0].text.strip()
-# print(title)
+titles = soup_nmn.find_all("div", class_="content_title")
+# titles = soup_nmn.find_all(target="_self")
+title = titles[1].text.strip()
+print(title)
 # print(soup_nmn.find(class_="content_title").find_all("p").text.strip())
 # print(soup_nmn.find(class_="article_teaser_body"))
 texts = soup_nmn.find_all("p")
@@ -71,6 +86,7 @@ BASE_PARAMS_NH = {
 
 r_mh = requests.get(BASE_URL_MH, params = BASE_URL_MH)
 # print(r_mh.status_code)
+# print(r_mh.text)
 
 soup_mh = BeautifulSoup(r_mh.text, "html.parser")
 # print(soup_mh.prettify())
@@ -81,5 +97,5 @@ soup_mh = BeautifulSoup(r_mh.text, "html.parser")
 # image = images.find("div", class_ = "item").find("img")["src"]
 
 images = soup_mh.find("div", class_ = "item")
-print(images.prettify())
+# print(images.prettify())
 # print(image.prettify())
