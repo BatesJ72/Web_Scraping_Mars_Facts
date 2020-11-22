851259441d365bb14c0adb0928656a6b5c6a48f3
Jessica Bates <bates.jessica72@gmail.com>
2020-11-21 19:48:17 -0600

    fuck yeah it works

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,37 @@
+from mission_to_mars_v2 import scrape
+from flask import Flask, jsonify
+import os
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+import pymongo
+
+
+CONN = os.getenv("MONGO_URI")
+client = pymongo.MongoClient(CONN)
+db = client.mars
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return "The app is up!"
+
+@app.route("/scrape_data")
+def scrape_data():
+    db.mars.insert_many(scrape())
+    return "Successfully scraped Mars data"
+
+@app.route("/all")
+def all_data():
+    results = []
+    for result in db.mars.find():
+        results.append({key: value for key, value in result.items() if not key == "_id"})
+    return jsonify(results)
+
+
+# @app.route("/fact_table")
+# def fact_table():
+#     return mars_fact_table.html
+
+if __name__ == "__main__":
+    app.run(debug = True)
